c88a4b4e75732a916883d781ee3e1d5bb26d1a7e
Marcel Müller <neikos@neikos.email>
2016-04-19 10:14:02 +0200

    Replace Hello World with PistonWindow Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 extern crate piston_window;
 extern crate camera_controllers;
 
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window : PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
+        .exit_on_esc(true).build().unwrap();
+    let mut events : WindowEvents = window.events();
+
+    while let Some(e) = events.next(&mut window) {
+        window.draw_2d(&e, |_c, g| {
+            clear([0.3, 0.85, 0.2, 1.0], g); // A lovely green
+        });
+    }
 }
